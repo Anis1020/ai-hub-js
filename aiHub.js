af7863399c94aec1fa0d4c9a6c291011c882ca38
Anisur Rahman <anis.dapunia@gmail.com>
2024-05-06 14:10:45 +0600

    shoe more button and id finding functionality added

--- aiHub.js
+++ aiHub.js
@@ -1,5 +1,7 @@
-const loadAllTools = async () => {
-  const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+const loadAllTools = async (searchText) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/ai/tools?search=${searchText}`
+  );
   const data = await res.json();
   const aiTools = data.data.tools;
   displayAllTools(aiTools);
@@ -7,8 +9,11 @@ const loadAllTools = async () => {
 
 const displayAllTools = (aiTools) => {
   const aiToolsContainer = document.getElementById("aiToolsContainer");
+  aiToolsContainer.innerHTML = "";
 
+  aiTools = aiTools.slice(0, 8);
   aiTools.forEach((aiTool) => {
+    console.log(aiTool);
     const aiToolsDiv = document.createElement("div");
     aiToolsDiv.classList = `card  bg-base-100 shadow-xl`;
     aiToolsDiv.innerHTML = `
@@ -16,10 +21,10 @@ const displayAllTools = (aiTools) => {
     <img src="${aiTool.image}" alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 class="card-title">${aiTool.name}</h2>
+    <p>${aiTool.description}</p>
     <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
+      <button onclick="showDetails('${aiTool.id}')" class="btn btn-primary">Show Details</button>
     </div>
   </div>
     `;
@@ -32,4 +37,10 @@ loadAllTools();
 const handleSearch = () => {
   const inputFieldText = document.getElementById("searchField");
   const searchText = inputFieldText.value;
+  loadAllTools(searchText);
+};
+
+// show details
+const showDetails = (id) => {
+  console.log("showDetails is clicked", id);
 };
