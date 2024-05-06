4c5016f73a0130a214a684de6e72146dc94ffcf4
Anisur Rahman <anis.dapunia@gmail.com>
2024-05-06 12:43:01 +0600

    loaded all data

--- aiHub.js
+++ aiHub.js
@@ -1,1 +1,6 @@
-console.log("ai hub is connected");
+const loadAllData = async () => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+  const data = await res.json();
+  console.log(data.data);
+};
+loadAllData();
